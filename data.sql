c5f0c0f72a19dab7b93390c20cb6c45e81278797
skyv26 <dev.aakashv@gmail.com>
2022-10-26 22:49:39 +0530

    Update `[data.sql]` by adding more data

--- data.sql
+++ data.sql
@@ -21,3 +21,16 @@ insert into animals
   ('Pikachu', '07-JAN-2021', 1, FALSE, 15.04),
   ('Devimon', '12-MAY-2017', 5, TRUE, 11)
 ;
+
+-- Insert more data 
+
+insert into animals
+  (name, date_of_birth, escape_attempts, neutered, weight_kg) values
+  ('Charmander', '08-FEB-2020', 0, false, -11),
+  ('Plantmon', '15-NOV-2021', 2, true, -5.7),
+  ('Squirtle', '02-APR-1993', 3, false, -12.13),
+  ('Angemon', '12-JUN-2005', 1, true, -45),
+  ('Boarmon', '07-JUN-2005', 7, true, 20.4),
+  ('Blossom', '13-OCT-1998', 3, true, 17),
+  ('Ditto', '14-MAY-2022', 4, true, 22)
+;
